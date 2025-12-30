5f9bc38afa0ae3914fff2df2e34b4bafd221073d
djacquin <danielcamilo853@gmail.com>
2025-12-30 07:40:33 -0500

    feat(app): compose app providers and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
+
+import ToastNotification from './components/ui/ToastNotification';
+import TankStackQueryProvider from './providers/TankStackQueryProvider';
+import store from './redux/store';
+import { router } from './routes/router';
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <>
+      <Provider store={store}>
+        <TankStackQueryProvider>
+          <ToastNotification />
+          <RouterProvider router={router} />
+        </TankStackQueryProvider>
+      </Provider>
+    </>
+  );
 }
 
 export default App;
